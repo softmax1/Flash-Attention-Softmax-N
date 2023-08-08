51c8a6e249d2616d8e88747ba89f7c222a8443d1
christopher-w-murphy <murphtron5000@gmail.com>
2023-08-08 18:49:41 -0400

    created more tests

--- tests/gpu/test_flash_attn_triton_og.py
+++ tests/gpu/test_flash_attn_triton_og.py
@@ -1,6 +1,7 @@
 from math import sqrt
 
-from torch import float16
+from pytest import mark
+from torch import float16, randn_like, bfloat16
 from torch.testing import assert_close
 
 from src.functional import slow_attention
@@ -8,16 +9,32 @@ from src.flash_attn_triton_og import attention
 from tests.common import get_query_key_value, device_name
 
 
-def test_attention(device_name):
+@mark.parametrize("dtype", [float16, bfloat16])
+@mark.parametrize("is_casual", [False, True])
+def test_attention(device_name, dtype, is_casual):
     batch_size = (32, 32)
     max_sequence_len = 1024
     embed_dimension = 32
 
-    dtype = float16
     atol = 1e-3
     rtol = 0.
 
+    # Test forward step,
     query, key, value = get_query_key_value(batch_size, max_sequence_len, embed_dimension, device=device_name, dtype=dtype)
-    actual = attention(query, key, value, False, 1 / sqrt(query.size(-1)))
-    expected = slow_attention(query, key, value)
+    actual = attention(query, key, value, is_casual, 1 / sqrt(query.size(-1)))
+    expected = slow_attention(query, key, value, is_causal=is_casual)
     assert_close(actual, expected, atol=atol, rtol=rtol)
+
+    # and backward step.
+    doutput = randn_like(query)
+    actual.backward(doutput)
+    actual_dvalue, value.grad = value.grad.clone(), None
+    actual_dkey, key.grad = key.grad.clone(), None
+    actual_dquery, query.grad = query.grad.clone(), None
+    expected.backward(doutput)
+    expected_dvalue, value.grad = value.grad.clone(), None
+    expected_dkey, key.grad = key.grad.clone(), None
+    expected_dquery, query.grad = query.grad.clone(), None
+    assert_close(actual_dvalue, expected_dvalue, atol=atol, rtol=rtol)
+    assert_close(actual_dkey, expected_dkey, atol=atol, rtol=rtol)
+    assert_close(actual_dquery, expected_dquery, atol=atol, rtol=rtol)
--- tests/gpu/test_flash_attn_triton_softmax1.py
+++ tests/gpu/test_flash_attn_triton_softmax1.py
@@ -1,4 +1,5 @@
-from torch import float16
+from pytest import mark
+from torch import float16, randn_like, bfloat16
 from torch.testing import assert_close
 
 from src.functional import slow_attention
@@ -6,16 +7,33 @@ from src.flash_attn_triton_softmax1 import attention
 from tests.common import get_query_key_value, device_name
 
 
-def test_attention(device_name):
+@mark.parametrize("dtype", [float16, bfloat16])
+@mark.parametrize("is_casual", [False, True])
+@mark.parametrize("scale", [None, 0.3])
+def test_attention(device_name, dtype, is_casual, scale):
     batch_size = (32, 32)
     max_sequence_len = 1024
     embed_dimension = 32
 
-    dtype=float16
     atol = 1e-3
     rtol = 0.
 
+    # Test forward step,
     query, key, value = get_query_key_value(batch_size, max_sequence_len, embed_dimension, device=device_name, dtype=dtype)
-    actual = attention(query, key, value)
-    expected = slow_attention(query, key, value, use_softmax1=True)
+    actual = attention(query, key, value, casual=is_casual, sm_scale=scale)
+    expected = slow_attention(query, key, value, is_causal=is_casual, scale=scale, use_softmax1=True)
     assert_close(actual, expected, atol=atol, rtol=rtol)
+
+    # and backward step.
+    doutput = randn_like(actual)
+    actual.backward(doutput)
+    actual_dvalue, value.grad = value.grad.clone(), None
+    actual_dkey, key.grad = key.grad.clone(), None
+    actual_dquery, query.grad = query.grad.clone(), None
+    expected.backward(doutput)
+    expected_dvalue, value.grad = value.grad.clone(), None
+    expected_dkey, key.grad = key.grad.clone(), None
+    expected_dquery, query.grad = query.grad.clone(), None
+    assert_close(actual_dvalue, expected_dvalue, atol=atol, rtol=rtol)
+    assert_close(actual_dkey, expected_dkey, atol=atol, rtol=rtol)
+    assert_close(actual_dquery, expected_dquery, atol=atol, rtol=rtol)
