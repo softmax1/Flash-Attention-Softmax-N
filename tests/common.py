78130d42fcecfa0e2a7ded044873b8300ccd7739
christopher-w-murphy <murphtron5000@gmail.com>
2023-08-08 09:06:20 -0400

    load all tensors to the same device

--- tests/common.py
+++ tests/common.py
@@ -1,6 +1,8 @@
 from typing import Optional, Tuple
 
+from pytest import fixture
 from torch import Tensor, rand
+from torch.cuda import is_available
 
 from src.functional import DType
 
@@ -15,3 +17,8 @@ def get_query_key_value(batch_size: int,
     key = rand(batch_size, max_sequence_len, embed_dimension, device=device, dtype=dtype)
     value = rand(batch_size, max_sequence_len, embed_dimension, device=device, dtype=dtype)
     return query, key, value
+
+
+@fixture(scope='session')
+def device_name() -> str:
+    return "cuda" if is_available() else "cpu"
--- tests/gpu/test_flash_attn_triton_softmax1.py
+++ tests/gpu/test_flash_attn_triton_softmax1.py
@@ -2,7 +2,7 @@ from torch.testing import assert_allclose
 
 from src.functional import slow_attention
 from src.flash_attn_triton_softmax1 import attention
-from tests.common import get_query_key_value
+from tests.common import get_query_key_value, device_name
 
 
 def test_attention():
@@ -10,7 +10,7 @@ def test_attention():
     max_sequence_len = 1024
     embed_dimension = 32
 
-    query, key, value = get_query_key_value(batch_size, max_sequence_len, embed_dimension)
+    query, key, value = get_query_key_value(batch_size, max_sequence_len, embed_dimension, device=device_name)
     actual = attention(query, key, value)
     expected = slow_attention(query, key, value, use_softmax1=True)
     assert_allclose(actual, expected)
