71686547e716499718cfeb10dde12abf79463b67
christopher-w-murphy <murphtron5000@gmail.com>
2023-08-08 23:29:15 -0400

    created simple test case with analytic expected answer

--- tests/gpu/test_flash_attn_triton_softmax1.py
+++ tests/gpu/test_flash_attn_triton_softmax1.py
@@ -1,5 +1,7 @@
+from math import exp
+
 from pytest import mark
-from torch import float16, randn_like
+from torch import float16, randn_like, ones, Tensor
 from torch.testing import assert_close
 
 from src.functional import slow_attention, slow_attention_redux
@@ -41,7 +43,7 @@ def test_attention(device_name, dtype, is_causal, scale):
 
 @mark.parametrize("dtype", [float16])
 @mark.parametrize("is_causal", [False, True])
-@mark.parametrize("scale", [None, 0.5])
+@mark.parametrize("scale", [None, 0.4])
 def test_attention_redux(device_name, dtype, is_causal, scale):
     batch_size = (32, 8)
     max_sequence_len = 1024
@@ -69,3 +71,35 @@ def test_attention_redux(device_name, dtype, is_causal, scale):
     assert_close(actual_dvalue, expected_dvalue, atol=atol, rtol=rtol)
     assert_close(actual_dkey, expected_dkey, atol=atol, rtol=rtol)
     assert_close(actual_dquery, expected_dquery, atol=atol, rtol=rtol)
+
+
+def test_simple_case(device_name):
+    N = 6
+    L = 1024
+    S = 1024 + 128
+    E = 64
+    Ev = 64
+    scale = 0.3
+    weight = 0.1
+
+    query = weight * ones((N, 1, L, E), device=device_name)
+    key = weight * ones((N, 1, S, E), device=device_name)
+    value = weight * ones((N, 1, S, Ev), device=device_name)
+
+    output_0a = slow_attention(query, key, value, scale=scale, use_softmax1=True)
+    output_1a = attention(query, key, value, False, scale)
+
+    expected_a_shape = weight * ones((N, 1, L, Ev), device=device_name)
+    expected_a_factor = S * exp(weight**2 * E * scale) / (1 + S * exp(weight**2 * E * scale))
+    expected_a = expected_a_shape * expected_a_factor
+
+    assert_close(output_0a, expected_a)
+    assert_close(output_1a, expected_a)
+
+    output_0b = slow_attention(query, key, value, scale=scale, is_causal=True, use_softmax1=True)
+    output_1b = attention(query, key, value, True, scale)
+
+    expected_b_factors = [(l + S - L) * exp(weight**2 * E * scale) / (1 + (l + S - L) * exp(weight**2 * E * scale)) for l in range(1, L + 1)]
+
+    assert_close(output_0b.sum(dim=0).sum(dim=-1), N * Ev * weight * Tensor([expected_b_factors]))
+    assert_close(output_1b.sum(dim=0).sum(dim=-1), N * Ev * weight * Tensor([expected_b_factors]))
