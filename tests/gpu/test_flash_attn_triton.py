6be9f79fb2acdc9665b1f6d8b1cfc527e22d360a
christopher-w-murphy <murphtron5000@gmail.com>
2023-08-13 12:00:51 -0400

    1st attempt at generalizing flast attention to softmax_n

--- tests/gpu/test_flash_attn_triton.py
+++ tests/gpu/test_flash_attn_triton.py
@@ -6,13 +6,14 @@ from torch.cuda import empty_cache
 from torch.testing import assert_close
 
 from src.functional import slow_attention_n
-from src.flash_attn_triton_softmax1 import attention
+from src.flash_attn_triton import flash_attention_n
 from tests.common import get_query_key_value, device_name, attention_analytic_answer, attention_analytic_casual_answer
 
 
+@mark.parametrize("sm_n", [0., 1., 1e-3, 1e-6, 4.])
 @mark.parametrize("is_causal", [False, True])
 @mark.parametrize("scale", [None, 0.5, 0.01])
-def test_flash_attention_comparison(device_name, is_causal, scale):
+def test_flash_attention_comparison(device_name, sm_n, is_causal, scale):
     """
     Compare the Torch and Triton Attention implementations
     """
@@ -25,8 +26,8 @@ def test_flash_attention_comparison(device_name, is_causal, scale):
 
     # Test forward step,
     query, key, value = get_query_key_value(batch_size, max_sequence_len, embed_dimension, device=device_name, dtype=float16)
-    actual = attention(query, key, value, is_causal, scale)
-    expected = slow_attention_n(query, key, value, is_causal=is_causal, scale=scale, n=1.)
+    actual = flash_attention_n(query, key, value, causal=is_causal, sm_scale=scale, sm_n=sm_n)
+    expected = slow_attention_n(query, key, value, is_causal=is_causal, scale=scale, n=sm_n)
     assert_close(actual, expected, atol=atol, rtol=rtol)
 
     # and backward step.
@@ -47,8 +48,9 @@ def test_flash_attention_comparison(device_name, is_causal, scale):
     gc.collect()
 
 
+@mark.parametrize("sm_n", [0., 1., 1e-3, 1e-6, 4.])
 @mark.parametrize("weight", [10, 3, 0.5, 0.04, 0.02, 0.01, 0, -0.01, -0.02, -0.04, -0.5, -3, -10])
-def test_flash_attention_analytic(device_name, weight):
+def test_flash_attention_analytic(device_name, sm_n, weight):
     """
     When the elements of the input tensors Q, K, & V all have the same value, there is a closed-form expression for the output of Attention w/ or w/o a casual mask.
     """
@@ -66,10 +68,10 @@ def test_flash_attention_analytic(device_name, weight):
     key = weight * ones((N, 1, S, E), device=device_name, dtype=float16)
     value = weight * ones((N, 1, S, Ev), device=device_name, dtype=float16)
 
-    output_0a = slow_attention_n(query, key, value, scale=scale, n=1.)
-    output_1a = attention(query, key, value, False, scale)
+    output_0a = slow_attention_n(query, key, value, scale=scale, n=sm_n)
+    output_1a = flash_attention_n(query, key, value, sm_scale=scale, sm_n=sm_n)
 
-    expected_a = attention_analytic_answer(N, L, S, E, Ev, scale, weight, softmax_n_param=1., device=device_name)
+    expected_a = attention_analytic_answer(N, L, S, E, Ev, scale, weight, softmax_n_param=sm_n, device=device_name)
 
     assert_close(output_1a, output_0a, atol=atol, rtol=rtol)
     assert_close(output_0a, expected_a, atol=atol, rtol=rtol)
@@ -78,11 +80,14 @@ def test_flash_attention_analytic(device_name, weight):
     atol = 0
     rtol = 2e-3
 
-    output_0b = slow_attention_n(query, key, value, scale=scale, is_causal=True, n=1.)
-    output_1b = attention(query, key, value, True, scale)
+    output_0b = slow_attention_n(query, key, value, scale=scale, is_causal=True, n=sm_n)
+    output_1b = flash_attention_n(query, key, value, causal=True, sm_scale=scale, sm_n=sm_n)
 
-    expected_b = attention_analytic_casual_answer(N, L, S, E, Ev, scale, weight, softmax_n_param=1., device=device_name)
+    expected_b = attention_analytic_casual_answer(N, L, S, E, Ev, scale, weight, softmax_n_param=sm_n, device=device_name)
 
     assert_close(output_1b, output_0b, atol=atol, rtol=rtol)
     assert_close(output_0b.sum(dim=0).sum(dim=-1), expected_b, atol=atol, rtol=rtol)
     assert_close(output_1b.sum(dim=0).sum(dim=-1), expected_b, atol=atol, rtol=rtol)
+
+    empty_cache()
+    gc.collect()
