c1946fabf29815baa25f1e50b8857afd6e1580e6
christopher-w-murphy <murphtron5000@gmail.com>
2023-09-05 11:38:30 -0400

    handle module not found error

--- flash_attention_softmax_n/surgery/__init__.py
+++ flash_attention_softmax_n/surgery/__init__.py
@@ -1,1 +1,5 @@
-from flash_attention_softmax_n.surgery.attention_softmax_n import AttentionSoftmaxN, apply_attention_softmax_n
+try:
+    from flash_attention_softmax_n.surgery.attention_softmax_n import AttentionSoftmaxN, apply_attention_softmax_n
+    SURGERY_INSTALLED = True
+except ModuleNotFoundError:
+    SURGERY_INSTALLED = False
--- flash_attention_softmax_n/surgery/surgery_functions/__init__.py
+++ flash_attention_softmax_n/surgery/surgery_functions/__init__.py
@@ -1,4 +1,7 @@
-from flash_attention_softmax_n.surgery.surgery_functions import _bert
-from flash_attention_softmax_n.surgery.surgery_functions.utils import policy_registry
+try:
+    from flash_attention_softmax_n.surgery.surgery_functions import _bert
+    from flash_attention_softmax_n.surgery.surgery_functions.utils import policy_registry
 
-__all__ = ['policy_registry']
+    __all__ = ['policy_registry']
+except ModuleNotFoundError:
+    __all__ = []
